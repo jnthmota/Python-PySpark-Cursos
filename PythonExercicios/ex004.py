db188ad1de39da40becc9ef62a8eb8bde2517e3f
jnthmota <jonathan.mota@outlook.com>
2021-06-03 16:18:22 -0300

    ex004

---
+++ PythonExercicios/ex004.py
@@ -0,0 +1,11 @@
+n = input('Digite Algo: ') # n = OBJETO
+r = n.istitle()
+print('O tipo primitivo desse valor é', type(n)) # TYPE MOSTRA O TIPO
+print('Tem espaço?  ', n.isspace()) #isspace() Método
+print('É um número?  ', n.isnumeric())
+print('É alfabético?  ', n.isalpha())
+print('É alfanúmerico?  ', n.isalnum())
+print('Está em maiúsculas?  ', n.isupper())
+print('Está em minúsculas?  ', n.islower())
+print('Está capitalizada? {}'.format(r)) # NOVO MODELO PYTHON3
+
