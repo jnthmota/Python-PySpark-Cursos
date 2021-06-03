d5a51f81ca48752d9ae87e61cc5b4ebf5ed36306
jnthmota <jonathan.mota@outlook.com>
2021-06-03 16:17:58 -0300

    ex003

---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,11 @@
+#n = float(input ('Digite um valor: '))
+#print(n)
+
+#n = input('Digite Algo: ')
+#print(n.isnumeric())  #SOMENTE NUMERO
+
+n = input('Digite Algo: ')
+print(n.isalpha()) # SOMENTE ALFA
+
+n = input('Digite Algo: ')
+print(n.isalnum()) # ALFA NUMERICO
