b0c706a4bcc7c6309d922dd14c7846d453c72569
jnthmota <jonathan.mota@outlook.com>
2021-06-03 16:16:49 -0300

    desafio001

---
+++ PythonExercicios/desafio001.py
@@ -0,0 +1,20 @@
+nome = input ('Qual seu nome?')
+idade = input ('Qual sua idade?')
+peso = input ('Qual seu peso?')
+print (nome, idade, peso)
+
+#DESAFIO1
+nome = input ('Qual seu nome?')
+print ("Ola",nome,"! Prazer em te conhecer")
+
+#DESAFIO02
+Dia = input ('Dia')
+Mes = input ('Mes')
+Ano = input ('Ano')
+print("Voce nasceu no dia",Dia,"de", Mes, "de", Ano, "Correto?")
+
+#DESAFIO03
+primeiro = int(input('Primeiro Número'))
+segundo = int(input ('Segundo Número'))
+print ("A soma é", primeiro + segundo)
+
