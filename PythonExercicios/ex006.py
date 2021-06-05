ba6e535ea6cbfab75c37b3a7bd924658f7a27639
jnthmota <jonathan.mota@outlook.com>
2021-06-05 14:02:04 -0300

    EX 006

--- PythonExercicios/ex006.py
+++ PythonExercicios/ex006.py
@@ -1,1 +1,7 @@
 #06)CRIE UM ALGORITMO QUE LEIA UM NÚMERO E MOSTRE O SEU DOBRO, TRIPLO E RAIZ QUADRADA
+
+a = int(input('Digite um valor: '))
+d = a * 2
+t = a * 3
+rq = a ** 2
+print('O seu dobro é: {} \n O seu triplo é: {} \n A sua raiz Quadrada é: {}'.format(d, t, rq))
