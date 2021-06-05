ddd5748483bae7db1c968c5ebd4dbd291f0d0ff9
jnthmota <jonathan.mota@outlook.com>
2021-06-05 12:18:30 -0300

    Operadores Aritméticos

--- Aula/Aula07.py
+++ Aula/Aula07.py
@@ -1,1 +1,31 @@
 #Operadores Aritméticos
+# DETALHE O OPERANDO PODE SER UMA STRING OU INT
+
+# + ADIÇÃO
+5+2 ==
+# - SUBTRAÇÃO
+5-2 ==
+# * MULTIPLICAÇÃO
+5*2 ==
+# / DIVISÃO
+5/2 ==
+# ** POTENCIA
+5**2 ==
+# // DIVISÃO INTEIRA
+5//2 ==
+# % RESTO DA DIVISÃO
+5%2 ==
+
+# ORDEM DE PRECEDENCIA
+# SIGNIFICA QUAL ORDEM FOI EXECUTADA PRIMEIRO
+
+1 ()
+2 **
+3 * / // %
+4 + -
+
+# EXEMPLOS
+
+5 + 3 * 2 == 11
+3 * 5 + 4 ** 2 == 31
+3 * (5+4) ** 2 = 243
