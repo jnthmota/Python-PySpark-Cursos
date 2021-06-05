fc9181dd90f487f3349d0c5bd6020a0bf222d432
jnthmota <jonathan.mota@outlook.com>
2021-06-05 14:11:38 -0300

    EX 007

--- PythonExercicios/ex007.py
+++ PythonExercicios/ex007.py
@@ -1,1 +1,6 @@
 #07) DESENVOLVA UM PROGRAMA QUE LEIA AS DUAS NOTAS DE UM ALUNO, CALCULE E MOSTRE A SUA MÉDIA
+n = float(input('Primeira nota: '))
+n1 = float(input('Segunda nota: '))
+r = (n + n1)/2
+print('Sua média é {:.3}'.format(r))
+
