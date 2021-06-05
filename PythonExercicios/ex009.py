42af2054ae982057b8ed7a653beb858efa74846d
jnthmota <jonathan.mota@outlook.com>
2021-06-05 14:47:10 -0300

    EX 009

--- PythonExercicios/ex009.py
+++ PythonExercicios/ex009.py
@@ -1,1 +1,15 @@
 #FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA
+
+t = int(input('Tabuada: '))
+r = t
+r1 = t*1
+r2 = t*2
+r3 = t*3
+r4 = t*4
+r5 = t*5
+r6 = t*6
+r7 = t*7
+r8 = t*8
+r9 = t*9
+r10 = t*10
+print('Tabuada do {} é: \n {}x1={} \n {}x2={} \n {}x3={} \n {}x4={} \n {}x5={} \n {}x6={} \n {}x7={} \n {}x8={} \n {}x9={} \n {}x10={}'.format(r, r, r1, r, r2, r, r3, r, r4, r, r5, r, r6, r, r7, r, r8, r, r9, r, r10))
