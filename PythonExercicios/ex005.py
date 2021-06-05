8d8df08fa72219a53d08c64d2ab6fce6cf160e51
jnthmota <jonathan.mota@outlook.com>
2021-06-05 13:49:29 -0300

    EX 005

--- PythonExercicios/ex005.py
+++ PythonExercicios/ex005.py
@@ -1,1 +1,6 @@
 #05)FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MOSTRE NA TELA O SEU SUCESSOR E SEU ANTECESSOR
+
+n = int(input('Digite um valor: '))
+a = n - 1
+s = n + 2
+print('Valor Digitado: {:^3} \n Seu antecessor: {} \n Seu sucessor: {:^5}'.format(n, a, s))
