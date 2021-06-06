cae979f4326e771ac08a1bebce4502aa0224ecd8
jnthmota <jonathan.mota@outlook.com>
2021-06-05 21:19:21 -0300

    EX 010

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,6 +1,5 @@
 #CRIE UM PROGRAMA QUE LEIA QUANTO DINHEIRO UMA PESSOA TEM NA CARTEIRA E MOSTRE QUANTOS DÓLARES ELA PODE COMPRAR
 
 d = float(input('Quanto dinheiro você tem em R$? '))
-r = d
 r1 = d / 5.05
-print('Você pode comprar com R${}, cerca de ${:.4} dólares'.format(r, r1))
+print('Você pode comprar com R${}, cerca de ${:.4} dólares'.format(d, r1))
