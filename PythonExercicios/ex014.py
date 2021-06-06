2f49f66f7b39d03e045a6397da37d50fe237c1f7
jnthmota <jonathan.mota@outlook.com>
2021-06-06 00:24:12 -0300

    EX 014

--- PythonExercicios/ex014.py
+++ PythonExercicios/ex014.py
@@ -1,1 +1,4 @@
 #14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+t = float(input('Digita a temperatura em ºC: '))
+f = ((9 * t) / 5) + 32
+print('Temperatura de {}ºC é {}ºF'.format(t, f))
