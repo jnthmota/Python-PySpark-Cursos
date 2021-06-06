8401300ce258d4a8db4dce26d00574f0b61e2e8d
jnthmota <jonathan.mota@outlook.com>
2021-06-05 21:16:18 -0300

    EX 008

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,1 +1,6 @@
 #ESCREVA UM PROGRAMA QUE LEIA UM VALOR EM METROS E O EXIBA CONVERTIDO EM CENTIMENTROS E MILIMETROS
+
+v = float(input('Digite um valor em metros: '))
+cm = v * 100
+mm = v * 1000
+print('A medida de {} metros convertida em \n cm é: {} \n mm é: {}'.format(v, cm, mm))
