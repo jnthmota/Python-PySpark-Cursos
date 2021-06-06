59dc8058c1d4eaebf6b669c9e2ab46bd96ce7e4e
jnthmota <jonathan.mota@outlook.com>
2021-06-05 22:09:59 -0300

    EX 012

--- PythonExercicios/ex012.py
+++ PythonExercicios/ex012.py
@@ -1,4 +1,4 @@
 #FAÇA UM ALGORITMO QUE LEIA O PREÇO DE UM PRODUTO E MOSTRE SEU NOVO PREÇO, COM 5% DE DESCONTO
-p = float(input('Qual é o preço do produto? '))
+p = float(input('Qual é o preço do produto? R$'))
 r1 = (p / 100) * 95
 print('O valor de R${} com 5% de desconto será de R${}'.format(p, r1))
