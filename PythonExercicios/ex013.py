d627e5294469778c115343a8688312109f2a87f5
jnthmota <jonathan.mota@outlook.com>
2021-06-05 22:10:25 -0300

    EX 013

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,5 +1,5 @@
 # FAÇA UM ALGORITMO QUE LEIA O SALÁRIO DE UM FUNCIONARIO E MOSTRE SEU NOVO SALÁRIO, COM 15% DE AUMENTO
 
-s = float(input('Qual e seu salário atual? '))
+s = float(input('Qual e seu salário atual? R$'))
 r1 = (s / 100) * 115
 print('O reajuste de 15% no seu salário atual de R${}, passará a ser de R${}'.format(s, r1))
