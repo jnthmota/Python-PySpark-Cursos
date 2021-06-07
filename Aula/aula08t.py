8d9521a01b3e48da16afa3a82ab994008b4d6abc
jnthmota <jonathan.mota@outlook.com>
2021-06-07 19:54:54 -0300

    Aula08

---
+++ Aula/aula08t.py
@@ -0,0 +1,4 @@
+#Nessa aula, vamos aprender como utilizar módulos em Python utilizando os comandos
+# import e from/import no Python. Veja como carregar bibliotecas de funções e utilizar
+# vários recursos adicionais nos seus programas utilizando módulos built-in e módulos
+# externos, oferecidos no Pypi.
