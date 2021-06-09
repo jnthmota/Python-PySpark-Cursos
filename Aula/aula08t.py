63da297ff4df9547381b60a6b09be6d0e3721baf
jnthmota <jonathan.mota@outlook.com>
2021-06-09 15:03:47 -0300

    AULA 08 TEORICA

--- Aula/aula08t.py
+++ Aula/aula08t.py
@@ -2,3 +2,21 @@
 # import e from/import no Python. Veja como carregar bibliotecas de funções e utilizar
 # vários recursos adicionais nos seus programas utilizando módulos built-in e módulos
 # externos, oferecidos no Pypi.
+
+import datetime #importa tudo do datetime
+from datetime import time # importa somente o time da biblioteca datetime
+
+math{
+    ceil #Redondamento para cima
+    floor #Redondamento para baixo
+    trunc #Truncante, truncar o número , elimina a virgula para frente
+    pow #Potência
+    sqrt #Calcular raiz quadrada
+    factorial # Calcular Fatorial
+}
+
+import math #Import tudo
+
+from math import pow #So importa pow
+from math import sqrt  #So importa sqrt
+from math import pow,sqrt # Importa as duas
