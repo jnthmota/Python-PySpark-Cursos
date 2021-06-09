6333f766c16e18f8344c71a3cc470ca4b4991518
jnthmota <jonathan.mota@outlook.com>
2021-06-09 15:32:32 -0300

    AULA 08 PRATICA

--- aula08p.py
+++ aula08p.py
@@ -1,5 +1,43 @@
 #Aula prática
-import math
+import math #tudo
 n = int(input('Digite um número: '))
 raiz = math.sqrt(n)
-print('A raiz do {} é igual a {}'.format(n, raiz))
+print('A raiz do {} é igual a {:.2f}'.format(n,raiz))
+
+# Arredondando para cima
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print('A raiz do {} é igual a {}'.format(n, math.ceil(raiz)))
+
+# Arredondando para baixo
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print('A raiz do {} é igual a {}'.format(n, math.floor(raiz)))
+
+# =======================================================
+
+from math import sqrt,floor #Importa somente o sqrt e floor
+
+n = int(input('Digite um número: '))
+raiz = sqrt(n)
+print('A raiz do {} é igual a {:.2f}'.format(n,raiz))
+
+# Arredondando para cima
+n = int(input('Digite um número: '))
+raiz = math.sqrt(n)
+print('A raiz do {} é igual a {}'.format(n, floor(raiz)))
+
+# =======================================================
+
+import random
+
+n = random.random()
+print(n)
+
+n = random.randint(1, 10) #numero inteiro
+print(n)
+
+# =======================================================
+
+import emoji
+print(emoji.emojize('Ola mundo :sunglasses: ', use_aliases=True))
