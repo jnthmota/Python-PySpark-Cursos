e74b10ab5bd11cbd725dd8b0dec04612bed68ea0
jnthmota <jonathan.mota@outlook.com>
2021-06-09 23:54:16 -0300

    EX 019

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -1,2 +1,10 @@
 ''' Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
  lendo o nome deles e escrevendo o nome do escolhido.'''
+import random
+a = str(input('Primeiro Aluno: '))
+b = str(input('Segundo Aluno: '))
+c = str(input('Terceiro Aluno: '))
+d = str(input('Quarto Aluno: '))
+lis = [a, b, c, d]
+r = random.choice(lis)
+print('O aluno escolhido foi: {} '.format(r))
