3411f20f00b9a5c8b7d6d9db7a8b6d16253bbbc1
jnthmota <jonathan.mota@outlook.com>
2021-06-09 22:53:40 -0300

    EX 019

--- PythonExercicios/ex019.py
+++ PythonExercicios/ex019.py
@@ -1,2 +1,2 @@
-# Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
-# lendo o nome deles e escrevendo o nome do escolhido.
+''' Um Professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
+ lendo o nome deles e escrevendo o nome do escolhido.'''
