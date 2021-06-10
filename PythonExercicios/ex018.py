3596afdcc927727d0fd789923c9b0ac71621f41b
jnthmota <jonathan.mota@outlook.com>
2021-06-09 22:53:17 -0300

    EX 018

--- PythonExercicios/ex018.py
+++ PythonExercicios/ex018.py
@@ -1,2 +1,2 @@
-#18 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno,cosseno e tangente
-#desse ângulo
+'''18 Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno,cosseno e tangente
+desse ângulo'''
