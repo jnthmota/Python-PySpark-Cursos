207fb7ecb0df7a716ffb332fd7bf00a580c4ec57
jnthmota <jonathan.mota@outlook.com>
2021-06-11 11:43:43 -0300

    EX 029

---
+++ PythonExercicios/ex029.py
@@ -0,0 +1,3 @@
+'''29 Escreva um programa que leia a velocidade de um carro.
+Se ele ultrapassar 80KM/h, mostre uma mensagem dizendo ele foi multado.
+A multa vai custar R$7,00 por cada KM acima do limite.'''
