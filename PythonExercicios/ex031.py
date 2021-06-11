1a3616a460ab94b7f4826feb80bea432b4e9a1f9
jnthmota <jonathan.mota@outlook.com>
2021-06-11 11:43:56 -0300

    EX 031

---
+++ PythonExercicios/ex031.py
@@ -0,0 +1,3 @@
+'''31 Desenvolva um programa que pergunte a distância de uma viagem em KM.
+Calcule o preço da passagem, cobrando R$0,50 por KM para viagens de até 200KM
+e R$0,45 para viagens mais longas'''
