2577413d8e2ca537f5e50d4eba359301f0f95674
jnthmota <jonathan.mota@outlook.com>
2021-06-11 10:31:31 -0300

    EX 020

--- PythonExercicios/ex020.py
+++ PythonExercicios/ex020.py
@@ -1,4 +1,4 @@
-'''' O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos
+'''' 20 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos
  Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.'''
 import random
 a = str(input('Primeiro aluno: '))
