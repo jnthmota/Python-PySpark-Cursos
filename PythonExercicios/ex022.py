87e8774ab10d1c6bb07e8feb9969f4a52def6278
jnthmota <jonathan.mota@outlook.com>
2021-06-10 21:36:18 -0300

    EX 022

---
+++ PythonExercicios/ex022.py
@@ -0,0 +1,12 @@
+'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+- O nome com todas as letras maiusculas
+- O nome com todas as letras minusculas
+- Quantas letras ao td (sem considerar o espaços)
+- Quantas letras tem o primeiro nome'''
+
+n = str(input('Digite o nome completo: '))
+div = n.split()
+print('Nome com todas as letras maiusculas: {}'.format(n.upper()))
+print('Nome com todas as letras minusculas: {}'.format(n.lower()))
+print('Quantas letras ao todo: {}'.format(len(n) - n.count(' ')))
+print('Seu nome {} tem {} letras no primeiro nome'.format(n, len(div[0])))
