a37ad2634a3ea6863c80e37e3d7ecb547e652f2b
jnthmota <jonathan.mota@outlook.com>
2021-06-10 22:27:44 -0300

    EX 024

---
+++ PythonExercicios/ex024.py
@@ -0,0 +1,3 @@
+'''Crie um programa que leia o nome de uma cidade e diga se ela começa ou não o nome "SANTO"'''
+ci = str(input('Digite um nome de uma cidade: ')).strip()
+print(ci[:5].lower() == 'santo')
