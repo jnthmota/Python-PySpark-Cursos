9d1a74d977dc6dd6179a3eb35cbd8947536d69a2
jnthmota <jonathan.mota@outlook.com>
2021-06-11 10:32:17 -0300

    EX 027

--- PythonExercicios/ex027.py
+++ PythonExercicios/ex027.py
@@ -1,4 +1,4 @@
-'''Faça um programa que leia o nome completo de uma pessoa. mostrando em seguida o primeiro e o
+'''27 Faça um programa que leia o nome completo de uma pessoa. mostrando em seguida o primeiro e o
 último nome separadamente'''
 nome = str(input('Digite seu nome completo: ')).strip()
 div = nome.split()
