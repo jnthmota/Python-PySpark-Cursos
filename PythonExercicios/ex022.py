d0bb46571bdb9f57fdf53f74660e7ef7e28ea74a
jnthmota <jonathan.mota@outlook.com>
2021-06-11 10:31:44 -0300

    EX 022

--- PythonExercicios/ex022.py
+++ PythonExercicios/ex022.py
@@ -1,4 +1,4 @@
-'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+'''22 Crie um programa que leia o nome completo de uma pessoa e mostre:
 - O nome com todas as letras maiusculas
 - O nome com todas as letras minusculas
 - Quantas letras ao td (sem considerar o espaços)
