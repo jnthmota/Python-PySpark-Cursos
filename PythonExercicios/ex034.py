71826c62cf3f949c0cbf78d1a62f21bafdc5ee36
jnthmota <jonathan.mota@outlook.com>
2021-06-11 11:44:14 -0300

    EX 034

---
+++ PythonExercicios/ex034.py
@@ -0,0 +1,3 @@
+'''34 Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
+Para salários superiores a R$1.250,00, calcule um aumento de 10%
+Para salários inferiores ou iguais,o aumento e de 15%'''
