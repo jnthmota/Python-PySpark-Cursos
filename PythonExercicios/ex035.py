02c0d2ea7f2db4f36d86d6ea77b9e9109a12dade
jnthmota <jonathan.mota@outlook.com>
2021-06-12 01:33:29 -0300

    EX 035

--- PythonExercicios/ex035.py
+++ PythonExercicios/ex035.py
@@ -1,2 +1,10 @@
 '''35 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem
 ou não formar um triângulo'''
+a = float(input('Digite a primeira reta: '))
+b = float(input('Digite a segunda reta: '))
+c = float(input('Digite a terceira reta: '))
+if a < b + c and b < a + c and c < a + b:
+    print('O comprimento dessas três retas pode formar um triângulo')
+else:
+    print('Infelizmente não pode formar um triângulo')
+
