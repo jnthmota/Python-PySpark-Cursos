9aaa08dc238a877bb29c07511dbb298aa7cbf4b8
jnthmota <jonathan.mota@outlook.com>
2021-06-11 23:59:49 -0300

    EX 031

--- PythonExercicios/ex031.py
+++ PythonExercicios/ex031.py
@@ -1,3 +1,10 @@
 '''31 Desenvolva um programa que pergunte a distância de uma viagem em KM.
 Calcule o preço da passagem, cobrando R$0,50 por KM para viagens de até 200KM
 e R$0,45 para viagens mais longas'''
+d = float(input('Qual a distância da viagem em KM? '))
+p =  d * 0.50
+p1 = d * 0.45
+if d > 200:
+    print('Você viagou {} KM, portanto irá pagar R${}'.format(d, p1))
+else:
+    print('Você viajou {} KM, portanto irá pagar R${:.2f} '.format(d, p))
