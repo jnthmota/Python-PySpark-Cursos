5eb12638086ac198da5da9b7b4696dde748eb944
jnthmota <jonathan.mota@outlook.com>
2021-06-11 23:46:47 -0300

    EX 030

--- PythonExercicios/ex030.py
+++ PythonExercicios/ex030.py
@@ -1,1 +1,7 @@
 '''30 Cria um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR'''
+nr = int(input('Digite um número: '))
+n = nr % 2
+if n == 0:
+    print('O número {} é PAR'.format(nr))
+else:
+    print('O número {} é IMPAR'.format(nr))
