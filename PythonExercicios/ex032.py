a78766efdc410c7bfc11d09465e7f5a46c41d34e
jnthmota <jonathan.mota@outlook.com>
2021-06-12 00:07:38 -0300

    EX 032

--- PythonExercicios/ex032.py
+++ PythonExercicios/ex032.py
@@ -1,1 +1,7 @@
 '''32 Faça um programa que leia um ano qualquer e mostre se ele e BISSEXTO'''
+ano = int(input('Digite o ano: '))
+c = ano % 4
+if c == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} não é BISSEXTO'.format(ano))
