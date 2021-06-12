6734b862ce1cd318dd2cfdd63798c263e8c8ec34
jnthmota <jonathan.mota@outlook.com>
2021-06-12 00:40:13 -0300

    EX 032

--- PythonExercicios/ex032.py
+++ PythonExercicios/ex032.py
@@ -5,8 +5,12 @@ De 100 em 100 anos não é ano bissexto.
 De 400 em 400 anos é ano bissexto.
 Prevalecem as últimas regras sobre as primeiras.
 '''
+import string
+from datetime import date
 ano = int(input('Digite o ano: '))
 c = ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0
+if ano == 0:
+    ano = date.today().year
 if c:
     print('O ano {} é BISSEXTO'.format(ano))
 else:
