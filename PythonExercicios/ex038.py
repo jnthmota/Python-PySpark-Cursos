ca7350f37a6d6b6f3eadbdfb3d386a98454f5cb5
jnthmota <jonathan.mota@outlook.com>
2021-06-14 14:31:03 -0300

    EX 038

---
+++ PythonExercicios/ex038.py
@@ -0,0 +1,14 @@
+'''
+38 Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+- O primeiro valor é maior
+- O segundo valor é maior
+- Não existe valor maior. os dois são iguais
+'''
+nr = int(input('Digite o primeiro número: '))
+nr1 = int(input('Digite o segundo número:  '))
+if nr > nr1:
+    print('-=-'*20)
+    print('O valor {} é MAIOR que {}'.format(nr, nr1))
+elif nr1 > nr:
+    print('-=-'*20)
+    print('O valor {} é MAIOR que {}'.format(nr1, nr))
