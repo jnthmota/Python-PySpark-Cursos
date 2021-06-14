343d31d8b811659ee87c0969840285673cf6b370
jnthmota <jonathan.mota@outlook.com>
2021-06-14 17:30:03 -0300

    EX 037

--- PythonExercicios/ex037.py
+++ PythonExercicios/ex037.py
@@ -5,3 +5,25 @@ a base de conversão:
 - 2 para octal
 - 3 para hexadecimal
 '''
+from time import sleep
+n = int(input('Digite qualquer número inteiro: '))
+p = int(input('Qual base de conversão você deseja? \n'
+              '1 - Binário\n'
+              '2 - Octal\n'
+              '3 - Hexadecimal\n'
+              'Digite sua escolha: '))
+binary = bin(n)
+octal = oct(n)
+hexadecimal = hex(n)
+print('-=-'*20)
+print('CONVERTANDO ... ')
+sleep(4)
+if p == 1:
+    print('O número digitado {} convertido para Binário ficou:\n'
+          '{}'.format(n, binary))
+elif p == 2:
+    print('O número digitado {} convertido para Octal ficou:\n'
+          '{}'.format(n, octal))
+elif p == 3:
+    print('O númnero digitado {} convertido para Hexadecimal ficou:\n'
+          '{}'.format(n, hexadecimal))
