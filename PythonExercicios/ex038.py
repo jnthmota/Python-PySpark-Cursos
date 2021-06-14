5964e3558032bcc82cd8fd6865e5be9e80b85fd1
jnthmota <jonathan.mota@outlook.com>
2021-06-14 14:51:14 -0300

    EX 038

--- PythonExercicios/ex038.py
+++ PythonExercicios/ex038.py
@@ -12,3 +12,7 @@ if nr > nr1:
 elif nr1 > nr:
     print('-=-'*20)
     print('O valor {} é MAIOR que {}'.format(nr1, nr))
+elif nr == nr1 and nr1 == nr:
+    print('-=-'*20)
+    print('Não existe valor MAIOR \n'
+          'O primeiro valor "{}" é igual ao segundo valor "{}"'.format(nr, nr1))
