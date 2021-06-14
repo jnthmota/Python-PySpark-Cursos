3bc3a20882041e34bdaf81903bb2f526219b7eec
jnthmota <jonathan.mota@outlook.com>
2021-06-14 14:31:35 -0300

    EX 043

---
+++ PythonExercicios/ex043.py
@@ -0,0 +1,9 @@
+'''
+43 Desenvolva uma lógica que leia o peso e a altura de uma pessoa,calcule seu IMC e mostre seu status,
+de acordo coma tabela abaixo:
+- Abaixo de 18.5: Abaixo do Peso
+- Entre 18.5 e 25: Peso ideal
+- 25 até 30: Sobrepeso
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade mórbida
+'''
