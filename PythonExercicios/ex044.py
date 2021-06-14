5317f14871b3fc837e77b848672b5e33ca5b524a
jnthmota <jonathan.mota@outlook.com>
2021-06-14 14:31:40 -0300

    EX 044

---
+++ PythonExercicios/ex044.py
@@ -0,0 +1,8 @@
+'''
+44 Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu
+preço normal e condição de pagamento:
+- À vista dinheiro/cheque: 10% de desconto
+- Á vista no cartão: 5% de desconto
+- Em até 2x no cartão: Preço normal
+- 3x ou mais no cartão: 20% de juros
+'''
