41503d2f9e6d27acd9c332a7a27b05c8edf083ae
jnthmota <jonathan.mota@outlook.com>
2021-06-14 14:31:29 -0300

    EX 042

---
+++ PythonExercicios/ex042.py
@@ -0,0 +1,7 @@
+'''
+42 Refaça o DESAFIO 035 dos triângulos, acrescentado o recurso de mostrar que tipo de triângulo será
+formado:
+- Equilátero: todos os lados iguais.
+- Isósceles: dois lados iguais.
+- Escaleno: todos os lados diferentes.
+'''
