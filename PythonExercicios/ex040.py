7bd924a7aff9fb7e413618eef4953bfe4d950656
jnthmota <jonathan.mota@outlook.com>
2021-06-14 15:01:25 -0300

    EX 040

--- PythonExercicios/ex040.py
+++ PythonExercicios/ex040.py
@@ -8,3 +8,18 @@ RECUPERAÇÃO
 - Média 7.0 ou superior:
 APROVADO
 '''
+a = float(input('Digite a primeira nota: '))
+b = float(input('Digite a segunda nota: '))
+c = (a + b) / 2
+if c < 5.0:
+    print('-=-'*20)
+    print('Sua média foi de {} \n'
+          'REPROVADO! Se dedique mais.'.format(c))
+elif c > 5.0 and c < 6.9:
+    print('-=-'*20)
+    print('Sua média foi de {} \n'
+          'RECUPERAÇÃO! Estude mais.'.format(c))
+elif c >= 7.0:
+    print('-=-'*20)
+    print('Sua média foi de {} \n'
+          'APROVADO! Parabéns ...'.format(c))
