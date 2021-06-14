ee0b7f9f0687e05aa1b4fae8a33d0bda39cdf6e1
jnthmota <jonathan.mota@outlook.com>
2021-06-14 15:31:38 -0300

    EX 041

--- PythonExercicios/ex041.py
+++ PythonExercicios/ex041.py
@@ -7,3 +7,22 @@ e mostre sua categoria, de acordo com a idade:
 - Até 20 anos: SÊNIOR
 - Acima: MASTER
 '''
+from datetime import date
+ano = int(input('Digite o ano de nascimento: '))
+atual = date.today().year
+idade = atual - ano
+if idade <= 9:
+    print('Você tem {} anos, portanto sua categoria é: \n'
+          'MIRIM'.format(idade))
+elif idade > 9 and idade <= 14:
+    print('Você tem {} anos, portanto sua categoria é: \n'
+          'INFANTIL'.format(idade))
+elif idade > 14 and idade <= 19:
+    print('Você tem {} anos, portanto sua categoria é: \n'
+          'JUNIOR'.format(idade))
+elif idade > 19 and idade <= 20:
+    print('Você tem {} anos, portanto sua categoria é: \n'
+          'SENIOR')
+elif idade > 20:
+    print('Você tem {} anos, portanto sua categoria é: \n'
+          'MASTER'.format(idade))
