be6bd1e7a57e651917c2e971f3a5434d856dbfa3
jnthmota <jonathan.mota@outlook.com>
2021-06-16 01:44:46 -0300

    EX 042

--- PythonExercicios/ex042.py
+++ PythonExercicios/ex042.py
@@ -5,3 +5,18 @@ formado:
 - Isósceles: dois lados iguais.
 - Escaleno: todos os lados diferentes.
 '''
+a = float(input('Digite a primeira reta: '))
+b = float(input('Digite a segunda reta: '))
+c = float(input('Digite a terceira reta: '))
+if a < b + c and b < a + c and c < a + b:
+    print('As retas digitadas podem formar um triângulo')
+    print('-=-'* 20)
+    if a == b == c:
+        print('As retas formam um "EQUILÁTERO"')
+    elif a != b != c != a:
+        print('As retas foram um "ESCALENO"')
+    else:
+        print('As retas formam um "ISÓSCELES"')
+else:
+    print('As retas não pode FORMAR um triângulo!')
+
