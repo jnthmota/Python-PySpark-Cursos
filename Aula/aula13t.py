311856bd0c4cec3baadff315a5da3395ea7c8470
jnthmota <jonathan.mota@outlook.com>
2021-06-17 11:15:53 -0300

    AULA 13 TEORICA

--- Aula/aula13t.py
+++ Aula/aula13t.py
@@ -8,4 +8,42 @@ estrutura versátil e simples de entender. Por exemplo:
 for c in range(0, 4):
 print(c)
 print(‘Acabou’)
+
+Laço com variavel de controle
+'''
+
+'''laco c no intervalo(1,10)
+     passo   
+   pega
+
+============================
+
+for c in range(1,10):
+    passo
+pega'''
+
+'''
+laço c no intervalo(0,3)
+    passo
+    pula
+passo
+pega
+
+==========================
+
+for c in range(0,3):
+   passo
+   pula
+passo
+pega
+'''
+
+'''
+laco c no intervalo(0,3)
+   if moeda 
+      pega
+   passo
+   pula
+passo
+pega
 '''
