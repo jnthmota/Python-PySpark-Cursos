e7f0103dc9d8d841415a50755d50d6ce5526d551
jnthmota <jonathan.mota@outlook.com>
2021-06-16 23:31:53 -0300

    AULA 13 TEORICA

---
+++ Aula/aula13t.py
@@ -0,0 +1,12 @@
+'''
+Repetições em Python (for)
+
+Nessa aula, vamos começar nossos estudos com os laços e vamos fazer primeiro o “for”, que é uma
+estrutura versátil e simples de entender. Por exemplo:
+
+for c in range(0, 4):
+
+print(c)
+
+print(‘Acabou’)
+'''
