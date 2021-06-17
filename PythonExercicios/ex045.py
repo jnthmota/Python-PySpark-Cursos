4a14ba0c93a8d8da882ef4291a18d8bc685d8fb9
jnthmota <jonathan.mota@outlook.com>
2021-06-16 22:00:46 -0300

    EX 045

--- PythonExercicios/ex045.py
+++ PythonExercicios/ex045.py
@@ -1,3 +1,18 @@
 '''
 45 Crie um programa que faça o computador jogar Jokenpô com você.
 '''
+from time import sleep
+from random import randint
+print('JOKENPÔ')
+print('-=-' * 20)
+po = ('Pedra', 'Papel', 'Tesoura')
+joke = randint(0, 2)
+escolhas = int(input('Faça uma escolha: \n'
+                     '1 - PEDRA\n'
+                     '2 - PAPEL\n'
+                     '3 - TESOURA\n'
+                     'ESCOLHA:  '))
+print('VALIDANDO ESCOLHAS  ...')
+sleep(3)
+print('Jokenpô escolheu {}'.format(po[joke]))
+print('Você escolheu {}'.format(po[escolhas]))
