b210269d4951c93e469fb640077d1a2d136642d4
jnthmota <jonathan.mota@outlook.com>
2021-06-16 22:16:06 -0300

    EX 045

--- PythonExercicios/ex045.py
+++ PythonExercicios/ex045.py
@@ -8,11 +8,38 @@ print('-=-' * 20)
 po = ('Pedra', 'Papel', 'Tesoura')
 joke = randint(0, 2)
 escolhas = int(input('Faça uma escolha: \n'
-                     '1 - PEDRA\n'
-                     '2 - PAPEL\n'
-                     '3 - TESOURA\n'
+                     '0 - PEDRA\n'
+                     '1 - PAPEL\n'
+                     '2 - TESOURA\n'
                      'ESCOLHA:  '))
-print('VALIDANDO ESCOLHAS  ...')
+print('JOKEENNN PÔ  ...')
 sleep(3)
-print('Jokenpô escolheu {}'.format(po[joke]))
-print('Você escolheu {}'.format(po[escolhas]))
+print('Jokenpô escolheu "{}"'.format(po[joke]))
+print('Você escolheu "{}"'.format(po[escolhas]))
+if joke == 0:
+    if escolhas == 0:
+        print('EMPATE')
+    elif escolhas == 1:
+        print('VOCÊ VENCEU')
+    elif escolhas == 2:
+        print('PERDEU')
+    else:
+        print('Opção inválida')
+elif joke == 1:
+    if escolhas == 0:
+        print('PERDEU')
+    elif escolhas == 1:
+        print('EMPATE')
+    elif escolhas == 2:
+        print('VOCÊ VENCEU')
+    else:
+        print('Opcão inválida')
+elif joke == 2:
+    if escolhas == 0:
+        print('VOCÊ VENCEU')
+    elif escolhas == 1:
+        print('PERDEU')
+    elif escolhas == 2:
+        print('EMPATE')
+    else:
+        print('Opção inválida')
