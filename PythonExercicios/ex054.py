68e3a3bd8d514fc069082356780311a97c2faaf4
jnthmota <jonathan.mota@outlook.com>
2021-06-17 12:57:56 -0300

    EX 054

---
+++ PythonExercicios/ex054.py
@@ -0,0 +1,5 @@
+'''
+54 Crie um programa que leia o ano de nascimento de sete pessoas.
+No final, mostre quantas pessoas ainda não atingiram a maioridade
+Quantas já são maiores a partir dos 21 anos.
+'''
