dcccd6f455075917d249ee8d80c3b1c5e0c90dad
jnthmota <jonathan.mota@outlook.com>
2021-06-17 12:57:51 -0300

    EX 053

---
+++ PythonExercicios/ex053.py
@@ -0,0 +1,10 @@
+'''
+53 Crie um programa que leia a frase qualquer e diga se ela é um palíndromo,
+desconsiderando os espaços.
+#palíndromo frase que pode ser lida de trás para frente
+ex: APOS A SOPA => APOS A SOPA Polindromo (true)
+A SACADA DA CASA
+A TORRE DA DERROTA
+O LOBO AMA O BOLO
+ANOTARAM A DATA DA MARATONA
+'''
