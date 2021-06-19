f3f58f50c5db0a9ff8ed03d881d24e252a7bd104
jnthmota <jonathan.mota@outlook.com>
2021-06-19 17:52:35 -0300

    EX 047

--- PythonExercicios/ex047.py
+++ PythonExercicios/ex047.py
@@ -1,7 +1,6 @@
 '''
 47  Crie um programa que mostre na tela todos os números pares, que estão no intervalo entre 1 e 50.
 '''
-for c in range(0, 51):
-    if c % 2 == 0:
+for c in range(2, 51, 2):
         print(c)
 print('-=-' * 20)
