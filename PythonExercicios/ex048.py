9ec756fda3d77f45357603c78ffe4e6c01eb914b
jnthmota <jonathan.mota@outlook.com>
2021-06-19 18:15:56 -0300

    EX 048

--- PythonExercicios/ex048.py
+++ PythonExercicios/ex048.py
@@ -2,3 +2,11 @@
 48 Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três
 e que se encontram no intervalo de 1 até 500.
 '''
+s = 0
+con = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        s += c
+        con +=  1
+print('A soma entre todos os números impares é {} \n'
+      'São múltiplos de três são {} '.format(s, con))
