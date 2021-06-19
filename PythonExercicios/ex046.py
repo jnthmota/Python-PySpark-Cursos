b0ccbe0761c0e7ff54441b03de7257145b64eaf2
jnthmota <jonathan.mota@outlook.com>
2021-06-18 23:22:47 -0300

    EX 046

--- PythonExercicios/ex046.py
+++ PythonExercicios/ex046.py
@@ -2,3 +2,9 @@
 46 Faça um programa que mostre na tela uma contagem regressiva para estouro de fogos de artificio,
 indo de 10 até 0, com uma pause de 1 segundo entre eles.
 '''
+import emoji
+from time import sleep
+for c in range(10, -1, -1):
+    sleep(1)
+    print(c)
+print(emoji.emojize("BOOOOOMMM :fireworks::fireworks::fireworks::fireworks:"))
