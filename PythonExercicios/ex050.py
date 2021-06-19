dd125d12ddce1abfc881174dd4427b115985af06
jnthmota <jonathan.mota@outlook.com>
2021-06-19 02:27:41 -0300

    EX 050

--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -3,3 +3,10 @@
 PARES.
 Se o valor digitado for ímpar,desconsidere-o.
 '''
+s = 0
+for n in range(1, 7):
+    a = int(input('Digite um número: '))
+    if a % 2 == 0:
+        s += a
+print('-=-' * 20)
+print('A soma dos números PARES ficou {}'.format(s))
