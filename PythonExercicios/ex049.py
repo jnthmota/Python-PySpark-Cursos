e0355ae677445e7ce5e2344a6087c0d2b96a5e49
jnthmota <jonathan.mota@outlook.com>
2021-06-18 23:53:42 -0300

    EX 049

--- PythonExercicios/ex049.py
+++ PythonExercicios/ex049.py
@@ -2,3 +2,8 @@
 49 Refaça o DESAFIO 009, mostrando a tabuada de um número que o usúario escolher, só que agora
 utilizando um laço for.
 '''
+tabuada = int(input('Tabuada: '))
+print('-=-' * 10)
+for t in range(1, 11):
+    r = tabuada * t
+    print('{}x{} = {}'.format(tabuada, t, r))
