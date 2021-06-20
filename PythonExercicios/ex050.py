7afa9e804b45c1b5db98a21f2e48311c4905fb73
jnthmota <jonathan.mota@outlook.com>
2021-06-19 23:06:34 -0300

    EX 050

--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -4,9 +4,12 @@ PARES.
 Se o valor digitado for ímpar,desconsidere-o.
 '''
 s = 0
+c = 0
 for n in range(1, 7):
     a = int(input('Digite o {}° número: '.format(n)))
     if a % 2 == 0:
         s += a
+        c += 1
 print('-=-' * 20)
-print('A soma dos números PARES ficou {}'.format(s))
+print('A você digitou {} números pares \n'
+      'Soma dos números PARES ficou {}'.format(c, s))
