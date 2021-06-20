c0a4c1eac0a3af6b6732a03855d51977cd32cc34
jnthmota <jonathan.mota@outlook.com>
2021-06-19 22:47:18 -0300

    EX 050

--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -5,7 +5,7 @@ Se o valor digitado for ímpar,desconsidere-o.
 '''
 s = 0
 for n in range(1, 7):
-    a = int(input('Digite um número: '))
+    a = int(input('Digite o {}° número: '.format(n)))
     if a % 2 == 0:
         s += a
 print('-=-' * 20)
