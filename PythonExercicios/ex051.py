7d7c5d18ad3281458a1ee9e197ee2994b399c69a
jnthmota <jonathan.mota@outlook.com>
2021-06-19 23:26:09 -0300

    EX 051

--- PythonExercicios/ex051.py
+++ PythonExercicios/ex051.py
@@ -2,3 +2,8 @@
 51 Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
 No final, mostre os 10 primeiros termos dessa progressão.
 '''
+p = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+for c in range(p, 11, r):
+    print('{}'.format(c))
+print('-=-' * 10)
