7fcfde929c4ac16cce428500d589cbd309ea7880
jnthmota <jonathan.mota@outlook.com>
2021-06-20 23:13:25 -0300

    EX 054

--- PythonExercicios/ex054.py
+++ PythonExercicios/ex054.py
@@ -3,3 +3,17 @@
 No final, mostre quantas pessoas ainda não atingiram a maioridade
 Quantas já são maiores a partir dos 21 anos.
 '''
+from datetime import date
+a = date.today().year
+mr = 0
+mn = 0
+for c in range(1,8):
+    l = int(input('Qual ano de nascimento da {}° pessoa?  '.format(c)))
+    idade = a - l
+    if idade >= 21:
+        mr += 1
+    else:
+        mn += 1
+print('-=-' * 20)
+print('Houveram \33[31m{}\33[m pessoas MAIORES de \33[31midade\33[m \n'
+      'Houveram \33[31m{}\33[m pessoas MENORES de \33[31midade\33[m'.format(mr, mn))
