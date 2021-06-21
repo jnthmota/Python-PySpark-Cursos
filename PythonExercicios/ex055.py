02adcb1563295138da8c776eadbbc64fcd875acf
jnthmota <jonathan.mota@outlook.com>
2021-06-20 23:35:31 -0300

    EX 055

--- PythonExercicios/ex055.py
+++ PythonExercicios/ex055.py
@@ -2,3 +2,19 @@
 55 Faça um programa que leia o peso de cinco pessoas.
 No final, mostre qual foi o MAIOR e o MENOR peso lidos.
 '''
+mr = 0
+mn = 0
+for p in range(1, 6):
+    peso = float(input('Qual é o peso da {}° pessoa? KG '.format(p)))
+    if p == 1:
+        mr = peso
+        mn = peso
+    else:
+        if peso > mr:
+            mr = peso
+        if peso < mn:
+            mn = peso
+print('-=-' * 13)
+print('O MAIOR PESO é: \33[31m{}\33[m\n'
+      'O MENOR PESO é: \33[31m{}\33[m'.format(mr, mn))
+print('-=-' * 13)
