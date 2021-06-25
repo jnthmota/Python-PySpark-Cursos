9f52f9625a76211c533c44c0f818d3107fbffe16
jnthmota <jonathan.mota@outlook.com>
2021-06-24 23:14:56 -0300

    EX 059

---
+++ PythonExercicios/ex059.py
@@ -0,0 +1,10 @@
+'''
+59 Crie um programa que leia dois valores e mostre um MENU na tela:
+[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos Números
+[5] Sair do programa
+
+Seu programa deverá realizar a operação solicitada em cada caso.
+'''
