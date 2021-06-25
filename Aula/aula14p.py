092b93634d7296583e5b2dac21bb4f2f1f78d15f
jnthmota <jonathan.mota@outlook.com>
2021-06-24 22:41:44 -0300

    AULA 14 PRATICA

--- Aula/aula14p.py
+++ Aula/aula14p.py
@@ -2,3 +2,24 @@
 Nessa aula, vamos continuar a estudar os laços e vamos aprender a usar a estrutura de repetição
 while no Python
 '''
+'''for c in range(1, 10):
+    print(c)
+print('Fim')'''
+
+'''c = 1
+while c < 10:
+    print(c)
+    c += 1
+print('Fim')'''
+# exemplo sem limite
+n = 1
+while n != 0: # Condição de parada
+    n = int(input('Digite um valor: '))
+print('Fim')
+
+# exemplo 2
+r = 'S'
+while r == 'S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+print('Fim')
