c682ecaaff555644f3c746879b8c4fa8ef3bb688
jnthmota <jonathan.mota@outlook.com>
2021-06-24 23:15:40 -0300

    EX 065

---
+++ PythonExercicios/ex065.py
@@ -0,0 +1,5 @@
+'''
+65 Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre
+a média entre todos os valores e qual foi o MAIOR e o MENOR valores lidos.
+O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+'''
