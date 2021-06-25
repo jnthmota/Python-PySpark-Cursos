989c9e2c285a36a35809d61c3c41c7762b179a10
jnthmota <jonathan.mota@outlook.com>
2021-06-24 22:49:38 -0300

    AULA 14 PRATICA

--- Aula/aula14p.py
+++ Aula/aula14p.py
@@ -23,3 +23,16 @@ while r == 'S':
     n = int(input('Digite um valor: '))
     r = str(input('Quer continuar? [S/N] ')).upper()
 print('Fim')
+
+# exemplo 3
+n = 1
+par = impar = 0
+while n != 0:
+    n = int(input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print('Você digitou {} PARES e {} IMPARES'.format(par, impar))
+
