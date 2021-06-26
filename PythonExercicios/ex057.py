7501cd2faab84008dd5be5dff7c43685b3e8120e
jnthmota <jonathan.mota@outlook.com>
2021-06-26 14:20:47 -0300

    EX 057

--- PythonExercicios/ex057.py
+++ PythonExercicios/ex057.py
@@ -2,3 +2,15 @@
 57 Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
 Caso esteja errado, peça a digitação novamente até ter um valor correto.
 '''
+sexo = ''
+while sexo != 'M' and sexo != 'F':
+    sexo = str(input('Qual é seu gênero? \n'
+                          '[M] Masculino\n'
+                          '[F] Feminino\n'
+                          'Digite sua escolha: ')).upper()
+    if sexo == 'M':
+        print('Seu gênero é \33[34mMasculino\33[m')
+    if sexo == 'F':
+        print('Seu gênero e \33[31mFeminino\33[m')
+    if sexo != 'M' and sexo != 'F':
+        print('\33[31mOpção inválida digite novamente!\33[m')
