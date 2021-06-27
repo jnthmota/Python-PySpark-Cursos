7aee87a50ae1b776010df280aaef8e74b909b4eb
jnthmota <jonathan.mota@outlook.com>
2021-06-27 01:26:39 -0300

    EX 058

--- PythonExercicios/ex058.py
+++ PythonExercicios/ex058.py
@@ -7,12 +7,15 @@ from time import sleep
 from random import randint
 c = randint(0, 10)
 d = ''
+con = 0
 while d != c:
     d = int(input('Qual número eu escolhi? '))
     print('\33[32mPROCESSANDO ...\33[m')
     sleep(1)
+    con += 1
     if c == d:
         print('Você \33[34m"ACERTOU"\33[m, Parabeéns!! o número foi {}!'.format(c))
+        print('Você tentou {} até acertar!'.format(con))
     else:
         print('Você \33[31m"ERROU"\33[m tente novamente!')
         print('-=-' * 10)
