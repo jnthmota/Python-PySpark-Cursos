c0483741dbe7510a314c06954857f303f58ba5dc
jnthmota <jonathan.mota@outlook.com>
2021-06-27 01:18:43 -0300

    EX 058

--- PythonExercicios/ex058.py
+++ PythonExercicios/ex058.py
@@ -3,3 +3,18 @@
 Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites
 foram necessários para vencer.
 '''
+from time import sleep
+from random import randint
+c = randint(0, 10)
+d = ''
+while d != c:
+    d = int(input('Qual número eu escolhi? '))
+    print('\33[31mPROCESSANDO ...\33[m')
+    sleep(1)
+    if c == d:
+        print('Você \33[34m"ACERTOU"\33[m {}!'.format(c))
+    else:
+        print('Você "ERROU" tente novamente!')
+        print('-=-' * 10)
+print('-=-' * 10)
+
