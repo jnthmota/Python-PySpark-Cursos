56ab5a62d67d5fd02829dfac913c990786dc5866
jnthmota <jonathan.mota@outlook.com>
2021-06-27 01:22:28 -0300

    EX 058

--- PythonExercicios/ex058.py
+++ PythonExercicios/ex058.py
@@ -9,12 +9,12 @@ c = randint(0, 10)
 d = ''
 while d != c:
     d = int(input('Qual número eu escolhi? '))
-    print('\33[31mPROCESSANDO ...\33[m')
+    print('\33[32mPROCESSANDO ...\33[m')
     sleep(1)
     if c == d:
-        print('Você \33[34m"ACERTOU"\33[m {}!'.format(c))
+        print('Você \33[34m"ACERTOU"\33[m, Parabeéns!! o número foi {}!'.format(c))
     else:
-        print('Você "ERROU" tente novamente!')
+        print('Você \33[31m"ERROU"\33[m tente novamente!')
         print('-=-' * 10)
 print('-=-' * 10)
 
