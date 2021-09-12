4106986b3ae48c7ed6d513b4acec741a52601dd2
jnthmota <jonathan.mota@outlook.com>
2021-09-12 17:45:35 -0300

    EX 060

--- PythonExercicios/ex060.py
+++ PythonExercicios/ex060.py
@@ -4,3 +4,9 @@ Ex
 5! = 5x4x3x2x1 = 120
 FOR e While
 '''
+from math import factorial
+n = int(input('Digite um número: '))
+f = factorial(n)
+print('#' * 40)
+print('O factorial de {} é {}'.format(n, f))
+print('#' * 40)
