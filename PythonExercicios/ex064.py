dee481b94df753efc422c4ef2aa0c510e8aab50d
jnthmota <jonathan.mota@outlook.com>
2021-09-12 19:41:00 -0300

    EX 064

--- PythonExercicios/ex064.py
+++ PythonExercicios/ex064.py
@@ -4,3 +4,14 @@ usuário digitar o valor 999, que é a condição de parada.
 No final, mostre quantos números foram digitados e qual foi a soma entre eles.
 (Desconsiderando o flag)
 '''
+n = con = soma = 0
+n = int(input('Digite um número: '))
+while n != 999:
+    print('[999] para parar')
+    soma += n
+    con += 1
+    n = int(input('Digite um número: '))
+print('Você digitou {} números e a soma deles foi {}'.format(con, soma))
+print('\33[31m#' * 7)
+print('# FIM #')
+print('#' * 7)
