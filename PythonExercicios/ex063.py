88698d9b3a0e1ec49a2c2bdb9583fb1740eeeaf8
jnthmota <jonathan.mota@outlook.com>
2021-09-12 18:57:42 -0300

    EX 063

--- PythonExercicios/ex063.py
+++ PythonExercicios/ex063.py
@@ -4,3 +4,17 @@ de uma Sequência de Fibonacci.
 Ex
 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 '''
+n = int(input('Digite um número inteiro: '))
+print('Sequência de Fibonacci do número {} ficou: '.format(n))
+print('#' * 50)
+t = 0
+t1 = 1
+print('{} \33[31m ==> \33[33m {}'.format(t, t1), end='')
+con = 3
+while con <= n:
+    t2 = t + t1
+    print('\33[31m ==> \33[33m {}'.format(t2), end='')
+    t = t1
+    t1 = t2
+    con += 1
+print('\33[31m ==> # FIM #\33[33m')
