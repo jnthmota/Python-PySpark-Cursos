914cc4ca744cb7c7dbb4fd71c8da083d149c8ae7
jnthmota <jonathan.mota@outlook.com>
2021-09-12 18:01:06 -0300

    EX 060

--- PythonExercicios/ex060.py
+++ PythonExercicios/ex060.py
@@ -5,8 +5,24 @@ Ex
 FOR e While
 '''
 from math import factorial
+from time import sleep
 n = int(input('Digite um número: '))
 f = factorial(n)
 print('#' * 40)
 print('O factorial de {} é {}'.format(n, f))
 print('#' * 40)
+
+'''using while'''
+n = int(input('Digite um número: '))
+con = n
+f = 1
+print('\33[31mCalculando [{}]....\33[33m'.format(n))
+while con > 0:
+    print('{}'.format(con), end='')
+    print(' x ' if con > 1 else ' = ', end='')
+    f *= con
+    con -= 1
+print('{}'.format(f))
+print('\33[31m#' * 7)
+print('# FIM #')
+print('#' * 7)
