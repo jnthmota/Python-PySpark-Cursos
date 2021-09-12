b8ef32a09d5436646351eaeac94b4b63b75c1122
jnthmota <jonathan.mota@outlook.com>
2021-09-12 18:15:38 -0300

    EX 061

--- PythonExercicios/ex061.py
+++ PythonExercicios/ex061.py
@@ -2,3 +2,14 @@
 61 Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
 da progressão usando a estrutura while.
 '''
+'''Exercicio 51 com for '''
+p = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+termo = p
+con = 1
+while con <= 10:
+    print('{} \33[31m ==> \33[33m'.format(termo), end='')
+    termo += r
+    con += 1
+print('\33[31m # FIM #\33[33m')
+
